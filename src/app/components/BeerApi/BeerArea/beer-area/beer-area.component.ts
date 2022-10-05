b289fd4b896c38b27cc3cec4a9e2d066d2ccb353
Justin Meskauskas <meskauskasjustinas@gmail.com>
2022-10-05 22:12:30 +0300

    Rendering any JSON object first layer, recursion for x layers is done

--- src/app/components/BeerApi/BeerArea/beer-area/beer-area.component.ts
+++ src/app/components/BeerApi/BeerArea/beer-area/beer-area.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Beer } from 'src/app/models/Beer';
 import { BeerService } from 'src/app/services/BeerServices/beer.service';
 
@@ -8,19 +9,65 @@ import { BeerService } from 'src/app/services/BeerServices/beer.service';
   templateUrl: './beer-area.component.html',
   styleUrls: ['./beer-area.component.css']
 })
-export class BeerAreaComponent implements AfterViewInit{
+export class BeerAreaComponent implements AfterViewInit, OnDestroy {
 
   beers: Beer[];
   display: any[];
-  selected: string = 'None';
+  selected: any = 'None';
+  properties: string[];
+  subscription: Subscription
+  propObj: Object
 
-  constructor(private beerService: BeerService) { }
+  constructor(private beerService: BeerService) {
+  }
 
   ngAfterViewInit(): void {
-    this.beerService.getBeer()
+    this.subscription = this.beerService.getBeer()
       .subscribe(response => {
         this.beers = response as Beer[]
+        this.properties = Object.getOwnPropertyNames(this.beers[0])
       })
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
+
+  parseBeerSelection(beer: Beer): string {
+    return beer[this.selected as keyof Beer] as string
+  }
+
+  recursiveObjectGeneration(obj: Object, stack: string): string {
+      const props = Object.getOwnPropertyNames(obj)
+      console.log('Props', props)
+      console.log('stack', stack)
+      let propObj = this.generateObjectFromProps(props)
+      console.log('propObj', propObj)
+      for (const prop of props) {
+        if(typeof obj[prop as keyof Object] === 'object') {
+          stack = stack + 'a'
+          console.log(prop)
+          this.recursiveObjectGeneration(obj[prop as keyof Object], stack)
+        }
+      }
+      //Object return key array
+      //If object return its key array
+      //Rinse and repeat
+      return JSON.stringify(this.propObj)
+  }
+
+  generateObjPropertyList(obj: any) : any {
+    return Object.keys(obj).map((key)=>{ return obj[key]});
+  }
+
+  generateObjectFromProps(input: any) {
+    const INITIAL_CHAR = 'a';
+    const INITIAL_CHAR_CODE = INITIAL_CHAR.charCodeAt(0);
+
+    const output = input.reduce((acc: { [x: string]: any; }, value: any, index: number) => {
+      const key = String.fromCharCode(INITIAL_CHAR_CODE + index);
+      acc[key] = value;
+      return acc;
+    }, {});
+  }
 }
